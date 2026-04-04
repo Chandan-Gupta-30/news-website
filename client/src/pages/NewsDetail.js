18e119047de1ebb7619f5a800fd32f96e4bab94e
Chandan-Gupta-30 <chandangupta2003.official@gmail.com>
2026-04-04 20:31:20 +0530

    fixed build error

--- client/src/pages/NewsDetail.js
+++ client/src/pages/NewsDetail.js
@@ -9,7 +9,7 @@ function NewsDetail() {
 
   useEffect(() => {
     fetchNews();
-  }, []);
+  }, [fetchNews]);
 
   const fetchNews = async () => {
     const res = await axios.get("http://localhost:5000/get-news");
